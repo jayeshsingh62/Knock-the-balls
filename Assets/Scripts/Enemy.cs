e464d754026d28d60fa8051e0447cb6ed5f9c05e
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-17 13:37:54 +0530

    Get the reference to the components

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,7 +10,8 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
