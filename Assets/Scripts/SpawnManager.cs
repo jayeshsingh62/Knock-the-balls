68edb94d33bd3cd849cb0f3b4c4eee8ebde8eedc
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-17 15:09:09 +0530

    Added a variable to store the spawn range

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float spawnRange = 9;
     // Start is called before the first frame update
     void Start()
     {
