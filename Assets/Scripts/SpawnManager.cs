4e677f3597a1849ca533d825f6539dfc13e5b455
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-17 15:17:39 +0530

    Created a new function to return the random position and use it in instantiate method

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,17 +8,23 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9;
     // Start is called before the first frame update
     void Start()
-    {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        Instantiate(enemyPrefab, randomPos, transform.rotation);
+    {      
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
     }
 }
