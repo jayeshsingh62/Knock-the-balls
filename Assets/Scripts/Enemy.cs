62c7db205ddf23e0cead98ec633dc951a4ba484d
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-17 13:37:03 +0530

    Added the variables

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 5;
+    private Rigidbody enemyRb;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
