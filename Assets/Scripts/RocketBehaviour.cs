854b7c0c26fb1a4d6c16bec6677bba55941a0b9f
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-26 17:53:51 +0530

    Added the variables

--- Assets/Scripts/RocketBehaviour.cs
+++ Assets/Scripts/RocketBehaviour.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class RocketBehaviour : MonoBehaviour
 {
+    private Transform target;
+    private float speed = 15.0f;
+    private bool homing;
+
+    private float rocketStrength = 15.0f;
+    private float aliveTimer = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
